939cb229b0134f6445aaadc4a5ce59bf0ff81913
kwerty2897 <teempmail12345@gmail.com>
2025-07-13 20:27:50 +0300

    com.zhiliaoapp.musically only

--- app/src/main/java/com/ussimspoofer/USSimSpoofer.java
+++ app/src/main/java/com/ussimspoofer/USSimSpoofer.java
@@ -7,40 +7,46 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class USSimSpoofer implements IXposedHookLoadPackage {
 
-    // Hardcoded spoofed SIM values (USA)
+    // Target SIM spoof values (USA)
     private static final String SIM_COUNTRY_ISO = "us";
     private static final String NETWORK_COUNTRY_ISO = "us";
     private static final String SIM_OPERATOR = "310004";
 
+    // Target package to spoof (change this to match your app)
+    private static final String TARGET_PACKAGE = "com.zhiliaoapp.musically";
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!lpparam.packageName.equals(TARGET_PACKAGE)) return;
+
         try {
             Class<?> telephonyClass = Class.forName(
-                    "android.telephony.TelephonyManager",
-                    false,
-                    lpparam.classLoader
+                "android.telephony.TelephonyManager",
+                false,
+                lpparam.classLoader
             );
 
-            // Spoof common telephony methods
+            // Hook methods to spoof SIM data
             hookMethod(telephonyClass, "getSimCountryIso", SIM_COUNTRY_ISO);
             hookMethod(telephonyClass, "getNetworkCountryIso", NETWORK_COUNTRY_ISO);
             hookMethod(telephonyClass, "getSimOperator", SIM_OPERATOR);
 
         } catch (Throwable t) {
-            // Error suppression
+            // Error suppression 
         }
     }
 
+    // Helper method to hook TelephonyManager methods
     private void hookMethod(Class<?> clazz, String methodName, final Object result) {
         XposedHelpers.findAndHookMethod(
-                clazz,
-                methodName,
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) {
-                        param.setResult(result);
-                    }
+            clazz,
+            methodName,
+            new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) {
+                    param.setResult(result);
                 }
+            }
         );
     }
 }
